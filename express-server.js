f13e9708c3c926d8eb7e6fece4a362edf324893a
mwkho <martin.h@hotmail.ca>
2021-03-15 22:38:31 +0000

    created a template to show the short and long urls, routed and rendered it

--- express-server.js
+++ express-server.js
@@ -24,6 +24,15 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// routing to a page with short and long url 
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  };  
+  res.render('urls_show', templateVars);  
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
